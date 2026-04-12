ed8f9a0c25aedf7642c270999f3356ed20252061
Visu <vikarna2426@gmail.com>
2026-04-12 22:11:07 +0530

    My 1st C code

---
+++ 1-sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    int sum = a + b;
+    printf("The sum of two numbers: %d\n", sum);
+    return 0;
+
+    
+}
