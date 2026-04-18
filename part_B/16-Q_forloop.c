8445e3760a9c9fb52bde2fc1b549faabb7749dd2
Vishwas27 <vikarna2426@gmail.com>
2026-04-18 23:52:20 +0530

    Add for loop problems

---
+++ part_B/16-Q_forloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Q) Print the number from 0 to 10?
+int main()
+{
+    for (int i = 0; i <= 10; i = i + 1) // we can write i++ for only i = i+1
+    {
+        printf("%d \n", i);
+    }
+
+    return 0;
+}
