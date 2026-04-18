142b6ba654c92e1583f637f9929eea35b64376ec
Vishwas27 <vikarna2426@gmail.com>
2026-04-18 23:50:26 +0530

    Add for loop basics

---
+++ part_B/14-forloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 18; i = i + 1)
+    {
+        printf("V will comeback! \n");
+    }
+    return 0;
+}
+
+//i= increment or counter variable
+// in for loop, the i value holds wirhin the the box({}) !
