314107fc47870d267d9f31e40bb67060409e3e1b
Vishwas27 <vikarna2426@gmail.com>
2026-04-21 23:34:33 +0530

    Implement of two functions and main function in a single C program

---
+++ part_C/23-functionn.c
@@ -0,0 +1,31 @@
+// Write 2 function one to print "Hello V" & second to print "good bye"?
+
+#include <stdio.h>
+
+void printHello();
+void printVsARC();
+void printGoodbye();
+
+
+int main()
+{
+    printHello();
+    printGoodbye();
+    printVsARC();
+    return 0;
+}
+
+void printHello()
+{
+    printf(" Hello V! \n");
+}
+
+void printGoodbye()
+{
+    printf("Good bye:) \n");
+}
+
+void printVsARC()
+{
+    printf("V's Arc !");
+}
