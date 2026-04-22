947842da86d9f7717bfd54cf0225cb43055ee884
Vishwas27 <vikarna2426@gmail.com>
2026-04-22 22:28:30 +0530

    Simple example of usage of function

---
+++ part_C/25-Q_function.c
@@ -0,0 +1,33 @@
+// Write a function that prints Namaste if user is Indian & Bonjour if user if French ?
+
+#include <stdio.h>
+
+void namaste();
+void bonjour();
+
+int main()
+{
+    printf("enter f for french & i for indian :");
+    char ch;
+    scanf("%c", &ch);
+
+    if (ch == 'i')
+    {
+        namaste();
+    }
+    else
+    {
+        bonjour();
+    }
+    return 0;
+}
+
+void namaste()
+{
+    printf("Namaste \n");
+}
+
+void bonjour()
+{
+    printf("Bonjour \n");
+}
