86ac956ab66dccce73bc5249f1f665dcda2a52a9
Vishwas27 <vikarna2426@gmail.com>
2026-04-26 15:41:52 +0530

    Note: You can iterate both int and char type elements

---
+++ part_C/29-loopcounter.c
@@ -0,0 +1,16 @@
+// loop counter can be float or even character !!!
+
+#include <stdio.h>
+int main()
+{
+    /* for (float i = 1.0; i <= 5.0; i++){
+       printf("%f \n", i);
+       } */
+    // this is float !
+
+    // for character
+    for (char ch = 'a'; ch <= 'z'; ch++)
+    {
+        printf("%c \n", ch);
+    }
+}
