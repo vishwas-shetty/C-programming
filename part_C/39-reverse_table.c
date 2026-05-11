ed41d29d11435d626a536b415249e19ffcb71e59
Vishwas27 <vikarna2426@gmail.com>
2026-05-11 21:33:45 +0530

    program to print multiplication table in reverse order

---
+++ part_C/39-reverse_table.c
@@ -0,0 +1,16 @@
+// print reverse of the table for a number n?
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the number:");
+    scanf("%d", &n);
+
+    for (int i = 10; i >= 1; i--)
+    {
+        printf("%d \n", n * i);
+    }
+    return 0;
+}
