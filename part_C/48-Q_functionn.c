f55eb8492f6b99f992398ef46fa6b7a00ada480a
Vishwas27 <vikarna2426@gmail.com>
2026-05-22 23:09:14 +0530

    square calculation using pow() from math library

---
+++ part_C/48-Q_functionn.c
@@ -0,0 +1,12 @@
+//Use library functions to calculate the square of a number given by user ?
+
+#include<stdio.h>
+#include<math.h>
+
+void calculatePrice(float valuse);
+
+int main(){
+int n = 4;
+printf("%f", pow(n,2));  //Power function  n=required number solution in problem and 2 is required sqaure value
+return 0;
+}
