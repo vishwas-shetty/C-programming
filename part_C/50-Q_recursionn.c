62fe28252370ac93858d91cc71bb61e21b2e5e66
Vishwas27 <vikarna2426@gmail.com>
2026-05-25 15:22:29 +0530

    Celsius to Fahrenheit conversion fuction using a recursion fuction

---
+++ part_C/50-Q_recursionn.c
@@ -0,0 +1,16 @@
+// Write a function to convert celcius to fahrenheit.
+
+#include<stdio.h>
+
+float convertTemp(float celsius);
+
+int main(){
+float far = convertTemp(27);
+printf("far : %f",far);
+return 0;
+}
+
+float convertTemp(float celsius){
+    float far = celsius * (9.0/5.0) + 32;
+    return far;
+}
